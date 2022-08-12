597dae5c88fcec77fdaa23963ef233f04be62c17
christy313 <christycreativity@gmail.com>
2022-08-12 10:07:30 -0400

    set up router

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const http = require("http");
+const detectRouter = require("./routes/detect");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -13,8 +15,15 @@ app.get("/", (req, res) => {
   res.send("hi");
 });
 
+app.use("/detect", detectRouter);
 app.set("port", port);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
+
+const server = http.createServer(app);
+server.listen(port);
+server.on("listening", () => {
+  console.log("Listening on " + port);
 });
